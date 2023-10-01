a1876aafce9703835de74c92cf39e5ceb6209c99
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-10-01 20:14:31 +0700

    add: styling logo, Home, dan About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>About</h1>
-    <h2>{{ $name }}</h2>
-    <h2>{{ $project }}</h2>
+<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+    <h1>Kelompok 7</h1>
+    <table>
+        <tr style="text-align: center;">
+            <th>NAMA</th>
+            <th>NIM</th>
+        </tr>
+        <tr>
+            <td>Adri Audifirst</td>
+            <td>24060121140152</td>
+        </tr>
+        <tr>
+            <td>Emerio Kevin Aryaputra</td>
+            <td>24060121120012</td>
+        </tr>
+        <tr>
+            <td>Mahardika Putra Wardhana</td>
+            <td>24060121130076</td>
+        </tr>
+        <tr>
+            <td>Mochammad Dzahwan Fadholy</td>
+            <td>24060121140168</td>
+        </tr>
+        <tr>
+            <td>Yusuf Muhammad Najhan</td>
+            <td>24060121130048</td>
+        </tr>
+    </table>
+    <p style="text-align: center;">
+        <em><strong>"Aku mah masih pemula, ajarin dong, puh sepuh. Puh sepuh, ajarin dong, masih pemula aku, puh sepuh. Tinky Winky, Dipsy, Lala, puh sepuh, ajarin dong, puh sepuh."</strong></em>
+    </p>
+</div>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">Bookorama</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-        aria-expanded="false" aria-label="Toggle navigation">
+    <a id="logo" class="navbar-brand custom-navbar-brand" href="https://www.gramedia.com" target="_blank">Bookorama</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item {{ $page_title == 'Home' ? 'active' : '' }}">
-                <a class="nav-link" href="/">Home</a>
+                <a class="nav-link" href="/" target="_blank">Home</a>
             </li>
             <li class="nav-item {{ $page_title == 'About' ? 'active' : '' }}">
                 <a class="nav-link" href="/about">About</a>
@@ -18,3 +17,11 @@
         </ul>
     </div>
 </nav>
+<style>
+    #logo {
+        border: 3px solid black;
+        border-radius: 10px;
+        background-color: pink;
+        padding: 10px;
+    }
+</style>
